7864c025fc74f88cdc2de55165c9d7465a5846f1
buffer <714433615@qq.com>
2020-07-06 09:46:34 +0800

    login

---
+++ app/Http/Controllers/Www/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by : PhpStorm
+ * User: yuansai chen
+ * Date: 2020/7/3
+ * Time: 17:08
+ */
+
+namespace App\Http\Controllers\Www;
+
+
+class HomeController extends BaseController
+{
+
+    public function home()
+    {
+        return 'welcome boy';
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::any('/', 'HomeController@home')->name('web-IndexHome');
+
+
+
