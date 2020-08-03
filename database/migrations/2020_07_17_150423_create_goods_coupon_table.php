44b2138c6dfac1b80475e98809184d4f29500bd0
buffer-pc <1324108818@qq.com>
2020-08-03 22:59:34 +0800

    test

--- database/migrations/2020_07_17_150423_create_goods_coupon_table.php
+++ database/migrations/2020_07_17_150423_create_goods_coupon_table.php
@@ -16,7 +16,7 @@ class CreateGoodsCouponTable extends Migration
         Schema::create('goods_coupon', function (Blueprint $table) {
             $table->increments('coupon_id')->comment('coupon_id');
             $table->string('coupon_name',30)->comment('券名称');
-            $table->decimal('condition_price',8,3)->comment('使用条件,满多少钱');
+            $table->decimal('condition_price',8,2)->comment('使用条件,满多少钱');
             $table->decimal('price',8,3)->comment("券面金额");
             $table->tinyInteger('is_del')->default(0)->comment('是否删除');
             $table->unsignedInteger('store_id')->default(0)->comment('店铺id');
