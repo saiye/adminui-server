291669c8b45394dda1495a1ab84e81af40cd8f53
buffer <714433615@qq.com>
2020-08-22 17:25:57 +0800

    about company

--- public/test.php
+++ public/test.php
@@ -1,16 +1,16 @@
 <?php
 
-$data=[
+/*$data=[
     'buys'=>[
 
     ],
     'user_coupon_id'=>0,
 ];
 $goodsIds=[
-  /*  [
+    [
         'id'=>1,
         'sku'=>[['tag_id'=>12,'sku_id'=>4],['tag_id'=>11,'sku_id'=>2]],
-    ],*/
+    ],
     [
         'id'=>2,
         'sku'=>[['tag_id'=>14,'sku_id'=>9],['tag_id'=>13,'sku_id'=>8]],
@@ -32,4 +32,33 @@ foreach ($goodsIds as $item){
     ]);
 }
 
-echo json_encode($data['buys']);
+echo json_encode($data['buys']);*/
+
+
+
+//消息
+$data=[
+    'channel'=>'sendMessage',
+    'code'=>'0',
+    'data'=>[
+        'nickname'=>'',
+        'sex'=>'',
+        'icon'=>'',
+        'type'=>1,//1文本，2图片，3表情,4定位,5送礼
+        'ext'=>[
+            'image'=>'',
+        ],
+    ],
+];
+
+//
+$data=[
+    'channel'=>'msgList',
+    'code'=>'0',
+    'data'=>[
+        [
+            'nickname'=>'',
+            'nickname'=>'',
+        ]
+    ],
+];
